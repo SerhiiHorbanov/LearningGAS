2fa814f6526feb4d921921cab028f49192f80018
Serhiiko <serikgorbanev@gmail.com>
2026-03-05 22:50:06 +0100

    Added project

---
+++ Source/LearningGAS/LearningGAS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LearningGAS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LearningGAS, "LearningGAS" );
